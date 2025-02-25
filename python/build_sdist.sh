d78b28e5ab6ca9d20b32688d4a280cf57d5f9094
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-02-24 17:40:38 -0800

    chore: enhance build_sdist.sh to improve directory structure and file copying
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- python/build_sdist.sh
+++ python/build_sdist.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 
-mkdir -p sentencepiece
+target_dir="$(dirname "$0")/sentencepiece"
+export target_dir
 
-for i in CMakeLists.txt LICENSE README.md VERSION.txt cmake config.h.in  sentencepiece.pc.in src third_party
+source_root_dir="$(dirname "$0")/.."
+export source_root_dir
+
+export target_src_dir="${target_dir}/python/src/sentencepiece"
+
+rm -rf "${target_dir}"
+mkdir -p "${target_src_dir}"
+
+for i in \
+  CMakeLists.txt LICENSE README.md \
+  VERSION.txt config.h.in sentencepiece.pc.in \
+  setup.py pyproject.toml MANIFEST.in uv.lock \
+  src third_party cmake
 do
-  echo "copying ../${i} sentencepiece/${i}"
-  cp -f -R "../${i}" sentencepiece
+  echo "copying ${source_root_dir}/${i} ${target_dir}/${i}"
+  cp -f -R "${source_root_dir}/${i}" "${target_dir}"
 done
 
-python3 setup.py sdist
+cp -f -R ./src/sentencepiece/*.py "${target_src_dir}/"
+cp -f -R ./src/sentencepiece/*.cxx "${target_src_dir}/"
+cp -f -R ./src/sentencepiece/*.i "${target_src_dir}/"
+(
+  cd sentencepiece || exit 5
+  uv run python setup.py sdist
+)
